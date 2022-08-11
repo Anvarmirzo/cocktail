a4ab406d4c1691ce35e23ca4ad7ccb36cf66b5b0
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-08-11 13:20:46 +0500

    header

---
+++ scripts/index.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+	(function slider() {
+		if (Swiper) {
+			const bannerSwiper = new Swiper('.banner-swiper', {
+					navigation: {
+						nextEl: '.banner-swiper .banner-swiper-next',
+						prevEl: '.banner-swiper .banner-swiper-prev'
+					}
+				}
+			);
+		} else {
+			console.warn('Swiper not found');
+		}
+	})();
+
+	(function dropdown() {
+		const dropdowns = document.querySelectorAll('.dropdown');
+
+		if (dropdowns?.length) {
+			dropdowns.forEach(dropdown => {
+				const btn = dropdown.querySelector('.dropdown__title');
+				const list = dropdown.querySelector('.dropdown-list');
+
+				btn.addEventListener('click', function () {
+					list.classList.toggle('active');
+				});
+			});
+		} else {
+			console.warn('Dropdown not found');
+		}
+	})();
+
+	(function burger() {
+		const btn = document.getElementById('burger-btn');
+		const menu = document.getElementById('mobile-menu');
+
+		if (btn && menu) {
+			btn.addEventListener('click', function () {
+				menu.classList.toggle('active');
+			});
+			menu.addEventListener('click', function (e) {
+				console.log(e);
+			})
+		} else {
+			console.warn('Burger or menu not found');
+		}
+	})();
+});
